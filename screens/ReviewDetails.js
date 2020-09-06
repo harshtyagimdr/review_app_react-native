b68c9e0a302970333cfa365eae21e6d47efb95e8
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-09-06 15:52:44 +0530

    Initial

---
+++ screens/ReviewDetails.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View,Text,StyleSheet} from 'react-native';
+
+export default function ReviewDetails(){
+    return(
+
+    <View>
+        <Text style={styles.container}>Review Screen</Text>
+    </View>
+    )
+}
+const styles=StyleSheet.create({
+    container:{
+        padding:24,
+    }
+})
