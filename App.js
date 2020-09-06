30e28b2e06dc46be80b0544a4dba5b1f48314894
harshtyagimdr <harsh.17bcs1046@abes.ac.in>
2020-09-06 16:21:03 +0530

    Stack Navigator added

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import Home from './screens/Home';
 import * as Font from 'expo-font';
 import {AppLoading} from 'expo';
+import Navigator from './routes/homeStack';
 
 const getFonts=()=>{
   return Font.loadAsync({
@@ -14,7 +15,7 @@ export default function App() {
   const [fontsLoaded,setFontsLoaded]=useState(false);
   if(fontsLoaded){
     return (
-      <Home/>
+      <Navigator/>
     );
   }else{
     return(
